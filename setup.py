c3f65dc596ae5db4bb426f3ef652e1c154c33af1
Yuval Peleg-Levy <yuvalp@ai21.com>
2024-01-22 09:44:06 +0200

    Added classifier adaptor; Made pip installable

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='web-table-classification',
+    version='1.0.0',
+    packages=find_packages(include=['pyjnius']),
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
