41cbae601871bfbcceda582e98a0ad0c45fcf3c8
Yuval Peleg-Levy <yuvalp@ai21.com>
2024-01-22 16:30:29 +0200

    Add init file

--- web_table_classification/dwtc_table_classifier.py
+++ web_table_classification/dwtc_table_classifier.py
@@ -1,8 +1,8 @@
 import jnius_config
 import os
 jar_file_path = f'{os.path.dirname(os.path.realpath(__file__))}/../runtime_testing/resources/dwtc-extension-1.0-jar-with-dependencies.jar'
-jnius_config.set_classpath(jar_file_path)
-
+if not jnius_config.vm_running:
+    jnius_config.set_classpath(jar_file_path)
 from jnius import autoclass
 TableClassifier = autoclass('webreduce.extension.classification.TableClassifier')
 table_classifier = TableClassifier()
