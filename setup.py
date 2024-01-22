fbea468a940b36b5b54cd039de72f8cd9595d7a3
Yuval Peleg-Levy <yuvalp@ai21.com>
2024-01-22 12:04:36 +0200

    Configured installation

--- setup.py
+++ setup.py
@@ -3,10 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name='web-table-classification',
     version='1.0.0',
-    packages=find_packages(include=['pyjnius']),
+    install_requires=['pyjnius'],
+    packages=find_packages(),
     url='',
     license='',
     author='',
     author_email='',
-    description=''
+    description='',
+#    package_data={'web_table_classification': ['runtime_testing/resources/dwtc-extension-1.0-jar-with-dependencies.jar']},
+    package_data={"": ["*.jar"]},
+
+    include_package_data = True
+    #data_files=[('resources', ['runtime_testing/resources/dwtc-extension-1.0-jar-with-dependencies.jar'])]
 )
---
+++ web_table_classification/dwtc_table_classifier.py
@@ -0,0 +1,17 @@
+import jnius_config
+import os
+jar_file_path = f'{os.path.dirname(os.path.realpath(__file__))}/../runtime_testing/resources/dwtc-extension-1.0-jar-with-dependencies.jar'
+jnius_config.set_classpath(jar_file_path)
+
+from jnius import autoclass
+TableClassifier = autoclass('webreduce.extension.classification.TableClassifier')
+table_classifier = TableClassifier()
+
+
+def classify_table_2_phase(table_html):
+    return table_classifier.classifyTable(table_html)
+
+if __name__ == '__main__':
+    with open(f'{os.path.dirname(os.path.realpath(__file__))}/../runtime_testing/resources/table.html') as f:
+        table_html = f.read()
+        print(classify_table_2_phase(table_html).tableType.toString())
